7f33e565493f2b3b9f9a97dbbc50d8d529a3c4e6
shailja8 <shailjatmr8822000@gmail.com>
2022-02-22 18:23:38 +0530

    Removed node_modules folder

--- controller/admin/CategoryController.js
+++ controller/admin/CategoryController.js
@@ -4,8 +4,10 @@ const path = require('path');
 exports.addCategory = (req,res,next)=>{
  const file = req.files.catImg;
  const fileName = new Date().getTime()+file.name;
+ console.log(req.files.catImg);
+ console.log(fileName);
   if(file){
-    const filePath  = path.join(__dirname,"../","public/images",fileName);
+    const filePath  = path.join(__dirname,"../../","Public/admin/images",fileName);
     file.mv(filePath,err=>{
       if(!err)
       {
@@ -14,15 +16,16 @@ exports.addCategory = (req,res,next)=>{
           let category = new Category(req.body.catName,fileName);
           category.saveCategory()
           .then(result=>{
+              console.log("inside returned promise");
               res.redirect("/admin/homepage");
           })
           .catch(err=>{
-              response.send("Error");
+              res.send(err);
           });
         }  
       }
       else
-        response.send("----Error----");
+        res.send(err);
     });
   }
 };
